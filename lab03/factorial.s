ae51d4d0aa2c60d30b69a615fe44ac99896bb0dc
dys <1115410174@qq.com>
2023-05-27 18:00:40 +0800

    finish lab 3

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -1,7 +1,7 @@
 .globl factorial
 
 .data
-n: .word 8
+n: .word 0
 
 .text
 # Don't worry about understanding the code in main
@@ -27,7 +27,15 @@ main:
 # The return value should be stored in a0
 factorial:
     # YOUR CODE HERE
-
+    addi t0,x0, 1
+    mv t1, a0
+loop:
+    beq t1, x0, finish
+    mul t0, t1, t0
+    addi t1, t1, -1
+    j loop
+finish:
+    mv a0, t0
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
     jr ra
--- lab03/fib.s
+++ lab03/fib.s
@@ -1,12 +1,12 @@
-.data
-n: .word 9
+.data # global variable are declared under the .data directive
+n: .word 9 # n is the name of the variable .word means that the size of the data is one word 9 is the value assigned to n
 
 .text
 main:
-    add t0, x0, x0 # curr_fib = 0
+    add t0, x0, x0 # curr_fib = 0 this is a comment
     addi t1, x0, 1 # next_fib = 1
-    la t3, n # load the address of the label n
-    lw t3, 0(t3) # get the value that is stored at the address denoted by the label n
+    la t3, n # load the address of the label n      t3 = &n
+    lw t3, 0(t3) # get the value that is stored at the address denoted by the label n t3 = *n
 fib:
     beq t3, x0, finish # exit loop once we have completed n iterations
     add t2, t1, t0 # new_fib = curr_fib + next_fib;
